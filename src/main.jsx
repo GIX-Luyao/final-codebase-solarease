8f4f977b92ea67715d3d97dca7ba797c996f0103
Xiao-Hannah <hannaxiao0628@gmail.com>
2026-02-01 12:25:01 -0800

    Add React Query for API caching with stale data prevention

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
 import App from './App'
 import './index.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
